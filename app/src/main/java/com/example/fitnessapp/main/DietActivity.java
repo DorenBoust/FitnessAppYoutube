e0d7908db14b8159dbaa116426eba736eb2eb68c
liordorenboust <liordorenboust@gmail.com>
2020-03-24 13:58:33 +0200

    Start Activity Diet, need to do: 1. add picture 2. alternative product 3. design

--- app/src/main/java/com/example/fitnessapp/main/DietActivity.java
+++ app/src/main/java/com/example/fitnessapp/main/DietActivity.java
@@ -1,9 +1,12 @@
 package com.example.fitnessapp.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.example.fitnessapp.R;
@@ -13,20 +16,21 @@ import com.example.fitnessapp.user.Product;
 import com.example.fitnessapp.user.ProductDataBase;
 import com.example.fitnessapp.user.User;
 
-import java.io.Serializable;
 import java.util.Dictionary;
 import java.util.List;
 
 public class DietActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView mealName;
+    TextView mealTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet);
 
-        textView = findViewById(R.id.product_title_diel);
+        mealName = findViewById(R.id.product_meal_name);
+        mealTime = findViewById(R.id.product_time_meal);
 
         Intent intent = getIntent();
         Meal meal = (Meal) intent.getSerializableExtra(KeysIntents.DIET_DATA);
@@ -39,7 +43,15 @@ public class DietActivity extends AppCompatActivity {
 
         ProductDataBase productDataBase1 = productDataBase.get(productName);
 
-        textView.setText(productDataBase1.getProductNameHEB());
+        final RecyclerView recyclerView = findViewById(R.id.recycler_diet_activity);
+        DietActivityRecycler adapter = new DietActivityRecycler(user,meal,getLayoutInflater());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutAnimation(AnimationUtils.loadLayoutAnimation(this,R.anim.layout_ex_activity_recycler_field));
+        recyclerView.setAdapter(adapter);
+
+
+        mealName.setText(meal.getName());
+        mealTime.setText(meal.getTime());
 
 
 
